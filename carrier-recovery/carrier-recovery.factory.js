c6b876ce31f689909c90456c4244a3aad54f6d7c
Robert Rypuła <robert.rypula@gmail.com>
2016-01-29 17:10:53 +0100

    carrier recovery update - phase information added

--- carrier-recovery/carrier-recovery.factory.js
+++ carrier-recovery/carrier-recovery.factory.js
@@ -14,21 +14,41 @@ var CarrierRecovery = (function () {
             this.$$sampleCount = 0;
             this.$$sampleHistory = [];
             this.carrierReferenceBPSKReal;
-            this.carrierReferenceBPSKIm;
+            this.carrierReferenceBPSKImm;
             this.carrierReferenceReal;
-            this.carrierReferenceIm;
+            this.carrierReferenceImm;
             this.carrierAvgBPSKReal;
-            this.carrierAvgBPSKIm;
+            this.carrierAvgBPSKImm;
             this.carrierAvgReal;
-            this.carrierAvgIm;
+            this.carrierAvgImm;
             this.carrierPowerBPSK;
             this.carrierPower;
             this.carrierPhaseBPSK;
             this.carrierPhase;
         };
 
-        CR.prototype.$$guessAngle = function (x, y) {
-            return 0.0;
+        CR.prototype.$$findUnitAngle = function (x, y) {
+            var length, q, angle;
+
+            length = Math.sqrt(x * x + y * y);
+            length = (length < 0.000001) ? 0.000001 : length;    // prevents from dividing by zero
+            q = (y >= 0) ? (x >= 0 ? 0 : 1) : (x < 0 ? 2 : 3);
+            switch (q) {
+                case 0:
+                    angle = Math.asin(y / length);
+                    break;
+                case 1:
+                    angle = Math.asin(-x / length) + 0.5 * Math.PI;
+                    break;
+                case 2:
+                    angle = Math.asin(-y / length) + Math.PI;
+                    break;
+                case 3:
+                    angle = Math.asin(x / length) + 1.5 * Math.PI;
+                    break;
+            }
+
+            return angle / (2 * Math.PI);
         };
 
         CR.prototype.$$computeReference = function () {
@@ -37,9 +57,9 @@ var CarrierRecovery = (function () {
                 x = omega * this.$$sampleCount;
 
             this.carrierReferenceBPSKReal = Math.cos(2 * x);
-            this.carrierReferenceBPSKIm = Math.sin(2 * x);
+            this.carrierReferenceBPSKImm = Math.sin(2 * x);
             this.carrierReferenceReal = Math.cos(x);
-            this.carrierReferenceIm = Math.sin(x);
+            this.carrierReferenceImm = Math.sin(x);
         };
 
         CR.prototype.$$addToHistory = function (sample) {
@@ -50,9 +70,9 @@ var CarrierRecovery = (function () {
                 sampleSquaredForBPSK: sampleSquaredForBPSK,
                 sampleNumber: this.$$sampleCount,
                 carrierBPSKReal: this.carrierReferenceBPSKReal * sampleSquaredForBPSK,
-                carrierBPSKIm: this.carrierReferenceBPSKIm * sampleSquaredForBPSK,
+                carrierBPSKImm: this.carrierReferenceBPSKImm * sampleSquaredForBPSK,
                 carrierReal: this.carrierReferenceReal * sample,
-                carrierIm: this.carrierReferenceIm * sample
+                carrierImm: this.carrierReferenceImm * sample
             });
 
             if (this.$$sampleHistory.length > SAMPLE_HISTORY_PERIOD_LENGTH * this.$$samplesPerPeriod) {
@@ -65,38 +85,38 @@ var CarrierRecovery = (function () {
 
             n = this.$$sampleHistory.length;
             this.carrierAvgReal = 0;
-            this.carrierAvgIm = 0;
+            this.carrierAvgImm = 0;
             this.carrierAvgBPSKReal = 0;
-            this.carrierAvgBPSKIm = 0;
+            this.carrierAvgBPSKImm = 0;
 
             for (i = 0; i < n; i++) {
                 sh = this.$$sampleHistory[i];
                 this.carrierAvgReal += sh.carrierReal;
-                this.carrierAvgIm += sh.carrierIm;
+                this.carrierAvgImm += sh.carrierImm;
                 this.carrierAvgBPSKReal += sh.carrierBPSKReal;
-                this.carrierAvgBPSKIm += sh.carrierBPSKIm;
+                this.carrierAvgBPSKImm += sh.carrierBPSKImm;
             }
 
             this.carrierAvgReal = this.carrierAvgReal / n;
-            this.carrierAvgIm = this.carrierAvgIm / n;
+            this.carrierAvgImm = this.carrierAvgImm / n;
             this.carrierAvgBPSKReal = this.carrierAvgBPSKReal / n;
-            this.carrierAvgBPSKIm = this.carrierAvgBPSKIm / n;
+            this.carrierAvgBPSKImm = this.carrierAvgBPSKImm / n;
         };
 
         CR.prototype.$$computePower = function () {
             this.carrierPower = 2.0 * Math.sqrt(
                 this.carrierAvgReal * this.carrierAvgReal +
-                this.carrierAvgIm * this.carrierAvgIm
+                this.carrierAvgImm * this.carrierAvgImm
             );
             this.carrierPowerBPSK = 4 * Math.sqrt(
                 this.carrierAvgBPSKReal * this.carrierAvgBPSKReal +
-                this.carrierAvgBPSKIm * this.carrierAvgBPSKIm
+                this.carrierAvgBPSKImm * this.carrierAvgBPSKImm
             );
         };
 
         CR.prototype.$$computePhase = function () {
-            this.carrierPhase = this.$$guessAngle(this.carrierAvgReal, this.carrierAvgIm);
-            this.carrierPhaseBPSK = this.$$guessAngle(this.carrierAvgBPSKReal, this.carrierAvgBPSKIm);
+            this.carrierPhase = this.$$findUnitAngle(this.carrierAvgReal, this.carrierAvgImm);
+            this.carrierPhaseBPSK = this.$$findUnitAngle(this.carrierAvgBPSKReal, this.carrierAvgBPSKImm);
         };
 
         CR.prototype.handleSample = function (sample) {
@@ -114,8 +134,13 @@ var CarrierRecovery = (function () {
         };
 
         CR.prototype.getCarrier = function () {
-            return this.carrierPower;
-            //return this.carrierPowerBPSK;
+            return {
+                phase: this.carrierPhase,
+                power: this.carrierPower,
+                powerBPSK: this.carrierPowerBPSK,
+                avgReal: this.carrierAvgReal,
+                avgImm: this.carrierAvgImm
+            };
         };
 
         CR.prototype.setSamplesPerPeriod = function (samplesPerPeriod) {
