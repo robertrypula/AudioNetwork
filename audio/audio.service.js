1e8ab520b84e1ac69b593010560228c95c93764e
Robert Rypuła <robert.rypula@gmail.com>
2016-02-18 12:49:08 +0100

    cross browser refactor, benchmark refactor

--- audio/audio.service.js
+++ audio/audio.service.js
@@ -54,41 +54,64 @@ var Audio = (function () {
         function userMediaStreamSuccess(stream) {
             var rawMicrofoneNode = context.createMediaStreamSource(stream);
 
+            /*
             microfoneSplitterNode = context.createChannelSplitter(2);
             microfoneMergerNode = context.createChannelMerger(2);
             rawMicrofoneNode.connect(microfoneSplitterNode);
             microfoneSplitterNode.connect(microfoneMergerNode, 0, 0);
             microfoneSplitterNode.connect(microfoneMergerNode, 0, 1);
             microfoneMergerNode.connect(microfoneNode);
+            */
+
+            rawMicrofoneNode.connect(microfoneNode);
+
+            console.log(rawMicrofoneNode);
+            console.log(microfoneNode);
         }
 
         function init() {
-            var AudioContext = window.AudioContext || window.webkitAudioContext;
-
-            context = new AudioContext();
-            microfoneNode = context.createGain();
-
-            if (!navigator.getUserMedia) {
-                navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-            }
-            navigator.getUserMedia(
-                {
-                    audio: {
-                        mandatory: {
-                            googEchoCancellation: false,
-                            googAutoGainControl: false,
-                            googNoiseSuppression: false,
-                            googHighpassFilter: false
-                        },
-                        optional: []
-                    }
-                },
-                userMediaStreamSuccess,
-                function(e) {
-                    alert('Error getting audio');
-                    console.log(e);
-                }
+            window.AudioContext = (function () {
+                return window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+            })();
+            navigator.getUserMedia = (
+                navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia
             );
+
+            try {
+                context = new window.AudioContext();
+            } catch (e) {
+                alert('Web Audio API is not supported in this browser');
+                console.log(e);
+            }
+
+            microfoneNode = context.createGain();
+            try {
+                navigator.getUserMedia(
+                    {
+                        video: false,
+                        audio: {
+                            mandatory: {
+                                googEchoCancellation: false,
+                                googAutoGainControl: false,
+                                googNoiseSuppression: false,
+                                googHighpassFilter: false
+                            },
+                            optional: []
+                        }
+                    },
+                    userMediaStreamSuccess,
+                    function (e) {
+                        alert('Error getting audio');
+                        console.log(e);
+                    }
+                );
+            } catch (e) {
+                alert('Microphone initialization failed')
+                console.log(e);
+            }
         }
 
         init();
