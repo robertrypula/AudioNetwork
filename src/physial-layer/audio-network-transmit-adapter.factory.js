e3d8138745e6274508a3e0a641184b145bc100f9
Robert Rypuła <robert.rypula@gmail.com>
2016-03-30 07:40:59 +0200

    Transmit refactor update

--- src/physial-layer/audio-network-transmit-adapter.factory.js
+++ src/physial-layer/audio-network-transmit-adapter.factory.js
@@ -3,13 +3,73 @@ var AudioNetworkTransmitAdapter = (function () {
 
     _AudioNetworkTransmitAdapter.$inject = [];
 
+    /**
+     * This works as an wrapper for raw API that AudioNetworkPhysicalLayter provides.
+     */
     function _AudioNetworkTransmitAdapter() {
         var ANTA;
 
         ANTA = function (audioNetworkPhysicalLayer) {
-            this.$$anpl = audioNetworkPhysicalLayer;
+            this.$$audioNetworkPhysicalLayer = audioNetworkPhysicalLayer;
         };
 
+        ANTA.prototype.$$transmit = function (channelIndex, data, amplitude, pskSize, symbolDuration, guardInterval, interpacketGap) {
+            var
+                symbolList, symbol,
+                txData, txDataTmp,
+                mute,
+                i, j
+            ;
+
+            txData = [];
+            for (i = 0; i < data.length; i++) {
+                symbolList = data[i];
+
+                if (symbolList.length !== amplitude.length) {
+                    throw 'Amplitude data length does not match symbol list length';
+                }
+
+                txDataTmp = [];
+                for (j = 0; j < symbolList.length; j++) {
+                    mute = symbolList[j] === null;
+                    symbol = mute ? 0 : parseInt(symbolList[j]) % pskSize;
+
+                    txDataTmp.push({
+                        amplitude: mute ? 0 : amplitude[j],
+                        duration: symbolDuration,
+                        phase: symbol / pskSize
+                    });
+                }
+                txData.push(txDataTmp);
+
+                if (guardInterval > 0) {
+                    txDataTmp = [];
+                    for (j = 0; j < symbolList.length; j++) {
+                        txDataTmp.push({
+                            amplitude: 0,
+                            duration: guardInterval
+                        });
+                    }
+                    txData.push(txDataTmp);
+                }
+            }
+
+            // add interpacket gap only when previous loop
+            if (interpacketGap > 0 && symbolList) {
+                txDataTmp = [];
+                for (j = 0; j < symbolList.length; j++) {
+                    txDataTmp.push({
+                        amplitude: 0,
+                        duration: interpacketGap
+                    });
+                }
+                txData.push(txDataTmp);
+            }
+
+            for (i = 0; i < txData.length; i++) {
+                this.$$audioNetworkPhysicalLayer.tx(channelIndex, txData[i]);
+            }
+        };
 
         ANTA.prototype.destroy = function () {
             
