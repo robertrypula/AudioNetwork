cfac31e0a841ef4020b09f7344466ea96a23a364
Robert Rypuła <robert.rypula@gmail.com>
2017-05-28 07:57:09 +0200

    Physical Layer update

--- example/00-000-common/physical-layer.js
+++ example/00-000-common/physical-layer.js
@@ -5,16 +5,19 @@ var PhysicalLayer;
 
 PhysicalLayer = function () {
     this.$$audioMonoIO = new AudioMonoIO(PhysicalLayer.RX_FFT_SIZE);
-    this.$$barkerCode = new BarkerCode(PhysicalLayer.RX_SAMPLE_FACTOR);
+    this.$$syncBarkerCode = new BarkerCode(PhysicalLayer.RX_SAMPLE_FACTOR);
     this.$$rxSmartTimer = new SmartTimer(PhysicalLayer.RX_TIME_MS);
     this.$$txSmartTimer = new SmartTimer(PhysicalLayer.TX_TIME_MS);
     this.$$txAmplitude = PhysicalLayer.TX_INITIAL_AMPLITUDE;
 
-    this.$$rxMode = null;
-    this.$$rxBinIndexData = null;
-    this.$$rxDataExponent = null;
-    this.$$rxDataValue = null;
-    this.$$rxOffset = 0;
+    this.$$rxMode;
+    this.$$rxBitPerDataSymbol;   // bits per data symbol
+    this.$$rxBinSizeData;        // data symbols (2^bitsPerData)
+    this.$$rxBinSizeTotal;       // data symbols + start frame + end frame
+    this.$$rxBinIndexFirst;      // index of first data symbol
+    this.$$rxBinIndexFrameStart; // index of start frame
+    this.$$rxBinIndexFrameEnd;   // index of end frame
+    this.$$rxOffset;
 
     this.$$txMode = null;
     this.$$txSampleRate = null;
@@ -32,6 +35,10 @@ PhysicalLayer.RX_TIME_MS = 0.1;
 PhysicalLayer.TX_TIME_MS = PhysicalLayer.RX_TIME_MS * PhysicalLayer.RX_SAMPLE_FACTOR;
 PhysicalLayer.TX_INITIAL_AMPLITUDE = 0.5;
 
+PhysicalLayer.CHANNEL_BIT_PER_DATA_SYMBOL = 6;
+PhysicalLayer.CHANNEL_A_FREQUENCY_BAND_START = 1000;
+PhysicalLayer.CHANNEL_B_FREQUENCY_BAND_START = 4200;
+
 PhysicalLayer.RX_MODE_DISABLED = 'RX_MODE_DISABLED';
 PhysicalLayer.RX_MODE_CHANNEL_A_NORMAL = 'RX_MODE_CHANNEL_A_NORMAL';
 PhysicalLayer.RX_MODE_CHANNEL_A_BARKER_CODE = 'RX_MODE_CHANNEL_A_BARKER_CODE';
@@ -44,7 +51,45 @@ PhysicalLayer.TX_MODE_CHANNEL_A_BARKER_CODE = 'TX_MODE_CHANNEL_A_BARKER_CODE';
 PhysicalLayer.TX_MODE_CHANNEL_B_NORMAL = 'TX_MODE_CHANNEL_B_NORMAL';
 PhysicalLayer.TX_MODE_CHANNEL_B_BARKER_CODE = 'TX_MODE_CHANNEL_B_BARKER_CODE';
 
+PhysicalLayer.UNSUPPORTED_RX_MODE = 'Unsupported RX mode';
+
 PhysicalLayer.prototype.setReceiverMode = function (rxMode) {
+    var
+        channelFrequencyBandStart;
+
+    switch (rxMode) {
+        case PhysicalLayer.RX_MODE_DISABLED:
+            break;
+        case PhysicalLayer.RX_MODE_CHANNEL_A_NORMAL:
+        case PhysicalLayer.RX_MODE_CHANNEL_A_BARKER_CODE:
+            channelFrequencyBandStart = PhysicalLayer.CHANNEL_A_FREQUENCY_BAND_START;
+            break;
+        case PhysicalLayer.RX_MODE_CHANNEL_B_NORMAL:
+        case PhysicalLayer.RX_MODE_CHANNEL_B_BARKER_CODE:
+            channelFrequencyBandStart = PhysicalLayer.CHANNEL_B_FREQUENCY_BAND_START;
+            break;
+        default:
+            throw PhysicalLayer.UNSUPPORTED_RX_MODE;
+    }
+
+    this.$$rxMode = rxMode;
+    if (this.$$rxMode === PhysicalLayer.RX_MODE_DISABLED) {
+        return;
+    }
+
+    this.$$rxBitPerDataSymbol = PhysicalLayer.CHANNEL_BIT_PER_DATA_SYMBOL;
+    this.$$rxBinSizeData = Math.pow(2, this.$$rxBitPerDataSymbol);
+    this.$$rxBinSizeTotal = this.$$rxBinSizeData + 2;
+    this.$$rxBinIndexFirst = FFTResult.getBinIndex(
+        channelFrequencyBandStart,
+        this.$$audioMonoIO.getSampleRate(),
+        PhysicalLayer.TX_FFT_SIZE // TX is fine here because we are down-converting rx decibel array before indexing
+    );
+    this.$$rxBinIndexFrameStart = this.$$rxBinIndexFirst + this.$$rxBinSizeData;
+    this.$$rxBinIndexFrameEnd = this.$$rxBinIndexFrameStart + 1;
+    this.$$rxOffset = 0;
+
+    console.log(this);
     /*
     44100 - 1024 - 43.06640625
     Channel A
