9ed5ff1c552d0e0960b1a8c41d3d6303ba3f07cd
Robert Rypuła <robert.rypula@gmail.com>
2017-08-20 23:57:05 +0200

    Physical Layer new example

---
+++ example/00-03-08-physical-layer-simplest/physical-layer-simplest.js
@@ -0,0 +1,50 @@
+// Copyright (c) 2015-2017 Robert Rypuła - https://audio-network.rypula.pl
+'use strict';
+
+var
+    physicalLayerBuilder,
+    physicalLayer;
+
+function init() {
+    physicalLayerBuilder = new PhysicalLayerBuilder();
+    physicalLayer = physicalLayerBuilder
+        .rxSymbolListener(rxSymbolListener)
+        .rxSampleListener(rxSampleListener)
+        .build();
+    html('#rx-sample-rate', physicalLayer.getRxConfig().sampleRate);
+}
+
+function onSetTxSampleRateClick() {
+    var txSampleRate = getValue('#tx-sample-rate', 'int');
+
+    physicalLayer.setTxSampleRate(txSampleRate);
+    alert('Tx Sample Rate set!');
+}
+
+function onSendSyncClick() {
+    physicalLayer.sendSync();
+}
+
+function onSendByteClick() {
+    var
+        byte = getValue('#tx-byte', 'int'),
+        txConfig = physicalLayer.getTxConfig(),
+        symbol = txConfig.symbolMin + byte;
+    physicalLayer.sendSymbol(symbol);
+}
+
+function rxSymbolListener(data) {
+    var rxConfig, byte;
+
+    rxConfig = physicalLayer.getRxConfig();
+    byte = data.symbol !== null
+        ? data.symbol - rxConfig.symbolMin
+        : null;
+    html('#rx-symbol', data.symbol !== null ? data.symbol : 'idle');
+    html('#rx-byte', byte !== null ? byte : '---');
+}
+
+function rxSampleListener(data) {
+    html('#sync', data.syncId === null ? 'waiting for sync...' : 'OK');
+    html('#sync-in-progress', data.isSyncInProgress ? '[sync in progress]' : '');
+}
