bbff38ed96619028fac6b0b2b6d02dde720ea5e1
Robert Rypuła <robert.rypula@gmail.com>
2018-04-27 23:37:43 +0200

    Index unit tests, package-lock.json fix

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,31 @@
 // Copyright (c) 2015-2018 Robert Rypuła - https://audio-network.rypula.pl
 
-import * as index from './index';
+import {
+  api,
+  COMMON_MODULE,
+  DSP_MODULE,
+  injector,
+  version
+} from './index';
 
 describe('index', () => {
   it('should give proper version', () => {
-    expect(index.version).toBe('2.0.0-rc');
+    expect(version).toBe('2.0.0-rc');
+  });
+
+  it('should lazy load CommonModule', () => {
+    const moduleMock = {};
+
+    spyOn(injector, 'get').and.returnValue(moduleMock);
+    expect(api.commonModule).toBe(moduleMock);
+    expect(injector.get).toHaveBeenCalledWith(COMMON_MODULE);
+  });
+
+  it('should lazy load DspModule', () => {
+    const moduleMock = {};
+
+    spyOn(injector, 'get').and.returnValue(moduleMock);
+    expect(api.dspModule).toBe(moduleMock);
+    expect(injector.get).toHaveBeenCalledWith(DSP_MODULE);
   });
 });
