47a709bd6192c1f8953c70579fb0e04e35702108
Robert Rypuła <robert.rypula@gmail.com>
2018-05-09 23:16:39 +0200

    Npm packages version update

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ function fillDev(config) {
 }
 
 function fillProd(config) {
-  config.mode = 'production';
+  config.mode = 'none';   // TODO: should be 'production'
   config.entry = {
     // TODO: BUG!! after introducing webpack 'modes' both files are minified - find the fix
     [`${packageName}-v${version}`]: './src/index.ts',
